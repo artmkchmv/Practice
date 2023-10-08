e673cf8e05d515574d841064ad3b06d4138689e1
Kuchumov <artemkakrut163@gmail.com>
2023-10-08 10:47:40 +0400

    IdentifyFunctionTest commit

---
+++ LR2/functions/IdentifyFunctionTest.java
@@ -0,0 +1,15 @@
+package LR2.functions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IdentifyFunctionTest {
+
+    @Test
+    public void testIdentifyFunction() throws Exception {
+        MathFunction func = new IdentifyFunction();
+        double result = func.apply(15.256789);
+        assertEquals(15.256789, result, 1e-9);
+    }
+}
