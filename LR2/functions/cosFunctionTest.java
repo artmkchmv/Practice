a1b6a578eb0c4853e1d16c1a9ee70cd986dbcf1d
Kuchumov <artemkakrut163@gmail.com>
2023-10-08 10:50:16 +0400

    cosFunctionTest commit

---
+++ LR2/functions/cosFunctionTest.java
@@ -0,0 +1,15 @@
+package LR2.functions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class cosFunctionTest {
+
+    @Test
+    public void testcosFunction() throws Exception {
+        MathFunction func = new cosFunction();
+        double result = func.apply(Math.PI / 3);
+        assertEquals(0.5, result, 1e-9);
+    }
+}
