0d56fadcd6cda172eb1e2dd36b2fa9b2107f302a
Kuchumov <artemkakrut163@gmail.com>
2023-10-09 21:14:17 +0400

    AbstractTabulatedFunction changes commit

--- LR2/functions/AbstractTabulatedFunction.java
+++ LR2/functions/AbstractTabulatedFunction.java
@@ -4,8 +4,7 @@ public abstract class AbstractTabulatedFunction implements TabulatedFunction {
     abstract protected int floorIndexOfX(double x);
     abstract protected double extrapolateLeft(double x);
     abstract protected double extrapolateRight(double x);
-
-    protected abstract double interpolate(double x, int floorIndex);
-    protected abstract double interpolate(double x, double leftX, double rightX, double leftY, double rightY);
-    public abstract double apply(double x);
+    abstract protected double interpolate(double x, int floorIndex);
+    abstract protected double interpolate(double x, double leftX, double rightX, double leftY, double rightY);
+    abstract public double apply(double x);
 }
