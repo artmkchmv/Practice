49a73cb97d4b9e4365cf2925b8a70c0be903c37d
Poverinov <Wolgrade@yandex.ru>
2023-10-09 17:51:17 +0400

    tgFunctionTest commit

---
+++ LR2/functions/tgFunctionTest.java
@@ -0,0 +1,15 @@
+package LR2.functions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class tgFunctionTest {
+
+    @Test
+    public void testtgFunction() throws Exception {
+        MathFunction func = new tgFunction();
+        double result = func.apply(Math.PI / 4);
+        assertEquals(1, result, 1e-9);
+    }
+}
