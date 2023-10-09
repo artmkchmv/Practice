714fddc2c92045beb27c838e0099d26681cb8bc7
Poverinov <Wolgrade@yandex.ru>
2023-10-09 18:02:39 +0400

    ConstantFunctionTest commit

---
+++ LR2/functions/ConstantFunctionTest.java
@@ -0,0 +1,22 @@
+package LR2.functions;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ConstantFunctionTest{
+
+    @Test
+    public void testConstantFunction_1() throws Exception {
+        ConstantFunction func = new ConstantFunction(0.9);
+        double result = func.apply(0.4);
+        assertEquals(0.9, result, 1e-9);
+    }
+    @Test
+    public void testConstantFunction_2() throws Exception {
+        ConstantFunction func = new ConstantFunction(0.9);
+        double result = func.getVal_x();
+        assertEquals(0.9, result, 1e-9);
+    }
+}
