3bfb97a7b10e7fe83aea090c10ec9eb069710183
Kuchumov <artemkakrut163@gmail.com>
2023-11-03 14:51:36 +0400

    Point create commit

---
+++ practice/src/main/java/ru/ssau/tk/oop/practice/functions/Point.java
@@ -0,0 +1,11 @@
+package ru.ssau.tk.oop.practice.functions;
+
+public class Point {
+    public final double x;
+    public final double y;
+
+    Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    };
+}
