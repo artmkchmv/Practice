4ff53ea622b9f6603ed5918e863ed84b8bb57ecc
Poverinov <Wolgrade@yandex.ru>
2023-11-06 14:18:10 +0400

    add to AbstractTabulatedFunction commit

--- practice/src/main/java/ru/ssau/tk/oop/practice/functions/AbstractTabulatedFunction.java
+++ practice/src/main/java/ru/ssau/tk/oop/practice/functions/AbstractTabulatedFunction.java
@@ -1,5 +1,8 @@
 package ru.ssau.tk.oop.practice.functions;
 
+import ru.ssau.tk.oop.practice.exceptions.DifferentLengthOfArraysException;
+import ru.ssau.tk.oop.practice.exceptions.ArrayIsNotSortedException;
+
 public abstract class AbstractTabulatedFunction implements TabulatedFunction {
     abstract protected int floorIndexOfX(double x);
 
@@ -12,4 +15,17 @@ public abstract class AbstractTabulatedFunction implements TabulatedFunction {
     abstract protected double interpolate(double x, double leftX, double rightX, double leftY, double rightY);
 
     abstract public double apply(double x);
+
+    public static void checkLengthIsTheSame(double[] xValues, double[] yValues) {
+        if (xValues.length != yValues.length) {
+            throw new DifferentLengthOfArraysException("different lenghts of arrays");
+        }
+    }
+
+    public static void checkSorted(double[] xValues) {
+        for (int i = 0; i < xValues.length - 1; i++) {
+            if (xValues[i] > xValues[i + 1])
+                throw new ArrayIsNotSortedException("Array not sorted");
+        }
+    }
 }
