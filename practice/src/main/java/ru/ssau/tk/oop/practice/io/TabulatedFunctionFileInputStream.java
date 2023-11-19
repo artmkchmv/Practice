986d2c58c6ca1ff46dfb24331d32862d036a572d
Kuchumov <artemkakrut163@gmail.com>
2023-11-19 22:44:00 +0400

    TabulatedFunctionFileInputStream/TabulatedFunctionFileOutputStream commit

---
+++ practice/src/main/java/ru/ssau/tk/oop/practice/io/TabulatedFunctionFileInputStream.java
@@ -0,0 +1,31 @@
+package ru.ssau.tk.oop.practice.io;
+
+import java.io.*;
+
+import ru.ssau.tk.oop.practice.functions.*;
+
+import ru.ssau.tk.oop.practice.functions.factory.*;
+
+import ru.ssau.tk.oop.practice.operations.*;
+
+public class TabulatedFunctionFileInputStream {
+    public static void main(String[] args) {
+        try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream("input/binaryfunction.bin"))) {
+            TabulatedFunctionFactory factory = new ArrayTabulatedFunctionFactory();
+            TabulatedFunction function = FunctionsIO.readTabulatedFunction(inputStream, factory);
+            System.out.println(function);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Введите размер и значения функции:\n");
+            TabulatedFunctionFactory factory = new LinkedListTabulatedFunctionFactory();
+            TabulatedDifferentialOperator operator = new TabulatedDifferentialOperator(factory);
+            TabulatedFunction function = FunctionsIO.readTabulatedFunction(inputReader, factory);
+            System.out.println(operator.derive(function).toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ practice/src/main/java/ru/ssau/tk/oop/practice/io/TabulatedFunctionFileOutputStream.java
@@ -0,0 +1,20 @@
+package ru.ssau.tk.oop.practice.io;
+
+import java.io.*;
+
+import ru.ssau.tk.oop.practice.functions.*;
+
+public class TabulatedFunctionFileOutputStream {
+    public static void main(String[] args) {
+        try (BufferedOutputStream ArrayFileOutputStream = new BufferedOutputStream(new FileOutputStream("output/arrayfunction.bin")); BufferedOutputStream LinkedListFileOutputStream = new BufferedOutputStream(new FileOutputStream("output/binaryfunction.bin"))) {
+            double[] xValues = {1.5, 2.6, 3.7};
+            double[] yValues = {7.1, 8.2, 9.3};
+            TabulatedFunction ArrayFunction = new ArrayTabulatedFunction(xValues, yValues);
+            TabulatedFunction LinkedListFunction = new LinkedListTabulatedFunction(xValues, yValues);
+            FunctionsIO.writeTabulatedFunction(ArrayFileOutputStream, ArrayFunction);
+            FunctionsIO.writeTabulatedFunction(LinkedListFileOutputStream, LinkedListFunction);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
