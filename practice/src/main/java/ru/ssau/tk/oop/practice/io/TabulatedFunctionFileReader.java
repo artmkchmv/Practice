599be927fcffe4ebca34259da31f88340fe92205
Poverinov <Wolgrade@yandex.ru>
2023-11-19 21:55:47 +0400

    TabulatedFunctionFileReader class commit

---
+++ practice/src/main/java/ru/ssau/tk/oop/practice/io/TabulatedFunctionFileReader.java
@@ -0,0 +1,28 @@
+package ru.ssau.tk.oop.practice.io;
+
+import ru.ssau.tk.oop.practice.functions.TabulatedFunction;
+import ru.ssau.tk.oop.practice.functions.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.oop.practice.functions.factory.LinkedListTabulatedFunctionFactory;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TabulatedFunctionFileReader {
+    public static void main(String[] args) {
+        try {
+            try (BufferedReader arrayReader = new BufferedReader(new FileReader("input/function.txt"));
+                 BufferedReader linkedListReader = new BufferedReader(new FileReader("input/function.txt"))) {
+                TabulatedFunction arrayFunction = FunctionsIO.readTabulatedFunction(arrayReader, new ArrayTabulatedFunctionFactory());
+                TabulatedFunction linkedListFunction = FunctionsIO.readTabulatedFunction(linkedListReader, new LinkedListTabulatedFunctionFactory());
+
+                System.out.println("Array function:");
+                System.out.println(arrayFunction);
+                System.out.println("Linked list function:");
+                System.out.println(linkedListFunction);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
