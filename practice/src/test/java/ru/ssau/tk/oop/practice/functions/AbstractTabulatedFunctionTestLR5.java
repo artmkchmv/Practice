f6f4a55cfd5616d328b9699dcefb468641030b01
Kuchumov <artemkakrut163@gmail.com>
2023-11-21 02:17:00 +0400

    AbstractTabulatedFunctionTestLR5 changes commit

--- practice/src/test/java/ru/ssau/tk/oop/practice/functions/AbstractTabulatedFunctionTestLR5.java
+++ practice/src/test/java/ru/ssau/tk/oop/practice/functions/AbstractTabulatedFunctionTestLR5.java
@@ -2,7 +2,7 @@ package ru.ssau.tk.oop.practice.functions;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AbstractTabulatedFunctionTestLR5 {
 
