9386447bc9fd06234125735512b5db181b6072f0
Kuchumov <artemkakrut163@gmail.com>
2023-11-24 23:11:24 +0400

    ReadTask change commit

--- practice/src/main/java/ru/ssau/tk/oop/practice/concurrent/ReadTask.java
+++ practice/src/main/java/ru/ssau/tk/oop/practice/concurrent/ReadTask.java
@@ -12,7 +12,7 @@ public class ReadTask implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < function.getCount(); i++) {
-                System.out.println("After read: i = " + i + ", x = " + function.getX(i) + ", y = " + function.getY(i));
+                System.out.printf("After read: i = %d, x = %f, y = %f", i, function.getX(i), function.getY(i));
         }
     }
 }
