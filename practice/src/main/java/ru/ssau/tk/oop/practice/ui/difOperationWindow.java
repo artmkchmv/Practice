23f6d7c1409e9362f3ca9aed7dcf172ed93d6768
Kuchumov <artemkakrut163@gmail.com>
2023-12-10 16:22:04 +0400

    ui update commit

---
+++ practice/src/main/java/ru/ssau/tk/oop/practice/ui/difOperationWindow.java
@@ -0,0 +1,10 @@
+package ru.ssau.tk.oop.practice.ui;
+
+import javax.swing.*;
+
+public class difOperationWindow extends JDialog {
+
+    public difOperationWindow(JFrame owner) {
+        super(owner, "Differentiation", true);
+    }
+}
