a8ac4b980ef08414195376d935ef5f3e3763f746
Poverinov <Wolgrade@yandex.ru>
2023-12-11 13:12:11 +0400

    Commit message of Error

---
+++ practice/src/main/java/ru/ssau/tk/oop/practice/ui/ErrorIlleagalArgumentWindow.java
@@ -0,0 +1,33 @@
+package ru.ssau.tk.oop.practice.ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ErrorIlleagalArgumentWindow extends JDialog {
+    public ErrorIlleagalArgumentWindow(JFrame owner) {
+        super(owner, "", true);
+        setSize(200, 100);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        JPanel errorPanel = new JPanel();
+        errorPanel.setLayout(new FlowLayout());
+
+        JLabel errorLabel = new JLabel("Invalid value!");
+        errorPanel.add(errorLabel);
+
+        JPanel okPanel = new JPanel();
+        okPanel.setLayout(new FlowLayout());
+
+        JButton okButton = new JButton("OK");
+        okPanel.add(okButton);
+
+        okButton.addActionListener(event -> setVisible(false));
+
+        add(errorPanel, BorderLayout.NORTH);
+        add(okPanel, BorderLayout.SOUTH);
+        setVisible(true);
+    }
+}
+
